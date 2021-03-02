de1817b07a0376875a539908ffb17455eaa890d3
danielVFS <danielv759153@gmail.com>
2021-03-02 14:25:13 -0300

    Add cors

--- backend/src/main/java/com/daniel/backend/resources/QuestionResource.java
+++ backend/src/main/java/com/daniel/backend/resources/QuestionResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.daniel.backend.domain.Question;
 import com.daniel.backend.services.QuestionService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/questions")
 public class QuestionResource {
