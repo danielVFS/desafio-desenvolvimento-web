10011929affc99078a3e8d2297fe3e03e23d01b7
danielVFS <danielv759153@gmail.com>
2021-03-02 18:01:59 -0300

    refactoe query to native query

--- backend/src/main/java/com/daniel/backend/repositories/QuestionRepository.java
+++ backend/src/main/java/com/daniel/backend/repositories/QuestionRepository.java
@@ -12,9 +12,9 @@ import org.springframework.data.jpa.repository.Query;
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Integer>{
 
-	@Query(nativeQuery=true, value="SELECT *  FROM Question ORDER BY random()") 
+	@Query(nativeQuery = true, value = "SELECT *  FROM Question ORDER BY random()") 
 	List<Question> findAllRandom();
 	
-	@Query("select question from Question question where question.difficulty = :difficulty")
+	@Query(nativeQuery = true, value = "SELECT * FROM Question WHERE DIFFICULTY = :difficulty")
 	List<Question> findAllByDifficulty(String difficulty);
 }
