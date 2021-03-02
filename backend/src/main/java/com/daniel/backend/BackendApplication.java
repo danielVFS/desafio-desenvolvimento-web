b00dedac5bb509a935d60eac12c667b8350e19b5
danielVFS <danielv759153@gmail.com>
2021-03-02 13:50:48 -0300

    testing relationships in Question

--- backend/src/main/java/com/daniel/backend/BackendApplication.java
+++ backend/src/main/java/com/daniel/backend/BackendApplication.java
@@ -1,13 +1,36 @@
 package com.daniel.backend;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BackendApplication {
+import com.daniel.backend.domain.Question;
+import com.daniel.backend.repositories.QuestionRepository;
 
+@SpringBootApplication
+public class BackendApplication implements CommandLineRunner{
+	
+	@Autowired
+	private QuestionRepository questionRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Set<String> incorrect_answers = new HashSet<String>();
+		incorrect_answers.add("2");
+		incorrect_answers.add("3");
+		
+		Question question = new Question(null, "Gerais", "easy", "qual foi ...", "1", incorrect_answers);
+		
+		this.questionRepository.saveAll(Arrays.asList(question));
 	}
 
 }
--- backend/src/main/java/com/daniel/backend/domain/Question.java
+++ backend/src/main/java/com/daniel/backend/domain/Question.java
@@ -29,4 +29,125 @@ public class Question implements Serializable{
 	
 	@ElementCollection
 	 private Set<String> incorrect_answers = new HashSet<>();
+
+	public Question() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Question(Integer id, String category, String difficulty, String question, String correct_question,
+			Set<String> incorrect_answers) {
+		super();
+		this.id = id;
+		this.category = category;
+		this.difficulty = difficulty;
+		this.question = question;
+		this.correct_question = correct_question;
+		this.incorrect_answers = incorrect_answers;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(String difficulty) {
+		this.difficulty = difficulty;
+	}
+
+	public String getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+
+	public String getCorrect_question() {
+		return correct_question;
+	}
+
+	public void setCorrect_question(String correct_question) {
+		this.correct_question = correct_question;
+	}
+
+	public Set<String> getIncorrect_answers() {
+		return incorrect_answers;
+	}
+
+	public void setIncorrect_answers(Set<String> incorrect_answers) {
+		this.incorrect_answers = incorrect_answers;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((category == null) ? 0 : category.hashCode());
+		result = prime * result + ((correct_question == null) ? 0 : correct_question.hashCode());
+		result = prime * result + ((difficulty == null) ? 0 : difficulty.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((incorrect_answers == null) ? 0 : incorrect_answers.hashCode());
+		result = prime * result + ((question == null) ? 0 : question.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Question other = (Question) obj;
+		if (category == null) {
+			if (other.category != null)
+				return false;
+		} else if (!category.equals(other.category))
+			return false;
+		if (correct_question == null) {
+			if (other.correct_question != null)
+				return false;
+		} else if (!correct_question.equals(other.correct_question))
+			return false;
+		if (difficulty == null) {
+			if (other.difficulty != null)
+				return false;
+		} else if (!difficulty.equals(other.difficulty))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (incorrect_answers == null) {
+			if (other.incorrect_answers != null)
+				return false;
+		} else if (!incorrect_answers.equals(other.incorrect_answers))
+			return false;
+		if (question == null) {
+			if (other.question != null)
+				return false;
+		} else if (!question.equals(other.question))
+			return false;
+		return true;
+	}
+	
+	
 }
